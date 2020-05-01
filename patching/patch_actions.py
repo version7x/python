6deccb100012f15cd732dfbd449569026c06931e
version7x <32801437+version7x@users.noreply.github.com>
2020-05-01 13:17:00 -0400

    version 1.0

--- patching/patch_actions.py
+++ patching/patch_actions.py
@@ -1,9 +1,29 @@
 #!/bin/env python
 
+from subprocess import Popen, PIPE
+from os import statvfs, getuid, uname
 from datetime import datetime
-from os import statvfs
-from subprocess import check_output
+from sys import exit
+import logging
 
+
+'''
+Patch_actions is a group of functions that is used in more than one 
+of the patching scripts to prevent code duplication.
+'''
+
+# Set up logging
+logger       = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+formatter    = logging.Formatter('%(asctime)-10s %(name)s %(levelname)s %(message)s')
+file_handler = logging.FileHandler('/var/log/patching.log')
+
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+
+# Set variables
 status_file = '/var/log/patching.status_file'
 log_file    = '/var/log/patching.log'
 
@@ -12,37 +32,41 @@ def update_status(action, outcome, details=None):
     Updates status file based on supplied values
     REQUIRES:  action, outcome, details(optional)
     PROVIDES:  tab delimted entry overwrite
+    RETURNS:   0 on sucess or '2, error' on failure
     '''
     tstamp  = datetime.now()
-    #logger.debug("Debug values - Time: {0}, action: {1},"
-    #    "outcome: {2}, details: {3}".format(tstamp, action, outcome, details))
+    logger.debug("Debug values - Time: {0}, action: {1},"
+        "outcome: {2}, details: {3}".format(tstamp, action, outcome, details))
     try:
         f      = open(status_file, 'w+')
-        f.write("{}\t{}\t{}\t{}".format(tstamp, action, outcome, details))
+        f.write("{0}\t{1}\t{2}\t{3}\n".format(tstamp, action, outcome, details))
     except Exception as e:
         f.close()
-        #logger.warning("Failed to update status file.  Error - {0}.\n"
-        #    "Values: {1}, {2}, {3}, {4}".format(e, tstamp, action, outcome, details))
+        logger.exception("Failed to update status file.  Error - {0}.\n"
+            "Values: {1}, {2}, {3}, {4}".format(e, tstamp, action, outcome, details))
         return 2, e
     finally:
         f.close
-        #logger.info("Status file updated: {0} {1} {2}".format(action, outcome, tstamp))
+        logger.info("Status file updated: {0} {1} {2}".format(action, outcome, tstamp))
         return 0
 
 def check_status():
     '''
     Returns the contencs of the status file or error
+    Returns contents of status file
+    CONTENTS:  timestamp action result error(optional)
+    RETURNS:   Status file or '2, error'
     '''
-    #logger.debug("Attempting ot read status file")
+    logger.debug("Attempting ot read status file")
     try:
         with open(status_file, 'r') as f:
             status = f.read()
         return status
     except FileNotFoundError:
-        #logger.debug("Status file does not exist")
+        logger.exception("Status file does not exist")
         return 2, "Status file does not yet exist"
     except Exception as e:
-        #logger.warning("Unknown filesystem error: {0}".format(e))
+        logger.exception("Unknown filesystem error: {0}".format(e))
         return 2, e
 
 def check_fs_size(partition, space):
@@ -63,5 +87,38 @@ def check_fs_size(partition, space):
         return "fail", b_avail
 
 def get_kernel_count():
-    count = check_output('rpm -qa --qf "%{NAME}\n" |grep -w "kernel"|grep -v "-"|wc -l', shell=True)
+    '''
+    Returns number of kernels installed
+    '''
+    output = Popen('rpm -qa --qf "%{NAME}\n" |grep -w "kernel"|grep -v "-"|wc -l', shell=True, stdout=PIPE, stderr=PIPE)
+    count, err = output.communicate()
     return count.strip()
+    
+def clean_kernels(num):
+    '''
+    Trims kernels to number provided
+    Similar to:  package-cleanup -y --oldkernels --count=${kern_num}
+    REQUIRES: number of kernels to keep
+    PROVIDES: list out put of pass/fail and command output
+    '''
+    kernels = Popen('/usr/bin/package-cleanup -y --oldkernels --count={0}'.format(num), shell=True, stdout=PIPE, stderr=PIPE)
+    ck_out, ck_err = kernels.communicate()
+    
+    if kernels.returncode != 0:
+        ck_fail = 'fail'
+        #logger.warning('Unable to clean kernels. Error: {0}'.format(ck_err))
+        return ck_fail, ck_err
+    else:
+        ck_fail = 'pass'
+        #logger.info('Kernels cleaned.  Trimmed down to {0}} kernels'.format(num))
+        #logger.info('Yum Output: {0}'.format(ck_out))
+        return ck_fail, ck_out
+
+def verify_root(test=False):
+        # Verify user is root
+        if getuid() != 0:
+            logger.critical("Attempted to run as non-root user")
+            print('You must be root to run this script')
+            exit(2)
+        else:
+            return 0
